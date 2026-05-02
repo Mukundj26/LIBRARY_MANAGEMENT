62b5e3ffda29c2d216077d9c9dbe217d37a6e53f
Mukundj26 <mukundjarad8@gmail.com>
2026-05-02 12:15:13 +0530

    first commit

--- LibraryManagement/app.py
+++ LibraryManagement/app.py
@@ -32,7 +32,8 @@ def create_app():
 
     return app
 
+# Expose 'app' globally for WSGI servers (like Vercel, Gunicorn)
+app = create_app()
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True)
